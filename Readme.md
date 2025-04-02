91e38f21dbe1c87cba9b807be5bf66600b86fbd5
yash agrawal <yashagrawaltech24@gmail.com>
2025-04-03 00:40:40 +0530

    Add Code

---
+++ Readme.md
@@ -0,0 +1,56 @@
+# Project Title: Weather Forecast Application Development in JavaScript
+
+## Objective
+The objective of this assignment is to develop a weather forecast application using JavaScript, HTML, and CSS. The application should retrieve weather data from an API, display it in a user-friendly interface, and provide essential features such as location-based forecasts, current weather conditions, and extended forecasts.
+
+## Tasks
+
+### 1. Set Up Project Structure (5 marks)
+- Create the basic project structure with HTML, CSS, and JavaScript files. ✅
+- Use **Tailwind CSS** in this project.
+- Set up a version control system (e.g., Git) for tracking changes throughout development.
+
+### 2. Integration with Weather API (10 marks)
+- Research and select a suitable weather API provider (e.g., OpenWeatherMap, WeatherAPI, etc.). ✅
+
+### 3. User Interface (UI) Design (25 marks)
+- Design a simple and intuitive user interface using HTML and Tailwind CSS. *(10 marks)* ✅
+- Ensure responsiveness for various screen sizes: 
+  - **Desktop**
+  - **iPad Mini**
+  - **iPhone SE** *(15 marks - 5 marks on each screen)*
+
+### 4. Location-Based Forecast (100 marks)
+- Allow users to search for weather forecasts by **city name**. *(15 marks)* ✅
+- Allow users to search weather forecasts for **current location**. *(15 marks)*
+- Add user interaction features such as buttons and input fields for selecting locations and viewing different weather data. *(10 marks)*
+- Implement dropdown menu for **recently searched cities**:
+  - Use **local or session storage** to store recently searched cities.
+  - Initially, the dropdown should be empty.
+  - Clicking on any city in the dropdown menu should update the weather data. *(20 marks)*
+- Implement event listeners to handle user interactions and update the UI accordingly. *(10 marks)*
+- Validate user inputs to prevent errors (e.g., invalid location, empty search queries). *(10 marks)*
+- Fetch and display **location-specific weather forecasts**: ✅
+  - Temperature
+  - Humidity
+  - Wind speed *(15 marks)*
+- Use appropriate icons or graphics to represent weather conditions (e.g., sunny, cloudy, rainy). *(5 marks)*
+
+### 5. Extended Forecast Display (30 marks)
+- Implement functionality to display **extended weather forecasts** for multiple days (e.g., 5-day forecast). *(15 marks)* ✅
+- Organize forecast data into a visually appealing and easy-to-read format: ✅
+  - Date
+  - Icon
+  - Temperature
+  - Wind speed
+  - Humidity *(15 marks)*
+
+### 6. Error Handling and Validation (20 marks)
+- Handle API errors gracefully and display appropriate error messages to users.
+
+### 7. Documentation (5 marks)
+- Document the codebase, including comments and a **README.md** file detailing setup instructions and usage.
+
+## Deliverables
+- Completed weather forecast application with all specified features implemented.
+- **GitHub repository** containing the project code with version control history. *(5 marks)*
---
+++ script.js
@@ -0,0 +1,137 @@
+const tempElement = document.querySelector("#temperature");
+const windElement = document.querySelector("#wind");
+const humidityElement = document.querySelector("#humidity");
+const cityElement = document.querySelector("#city-name");
+const imgElement = document.querySelector("#image");
+const dateElement = document.querySelector("#date");
+const cardsContainer = document.querySelector(".cards");
+const form = document.querySelector("#form");
+const useLocationBtn = document.querySelector("#use-location-btn");
+const errorElement = document.querySelector("#error-elem");
+
+const setErrorMessage = (message) => {
+    errorElement.classList.remove("hidden");
+    errorElement.classList.add("inline-block");
+    errorElement.textContent = message;
+};
+
+const UpdateUI = (name, temperature, humidity, wind, img) => {
+    tempElement.textContent = `${temperature} c`;
+    humidityElement.textContent = humidity;
+    windElement.textContent = `${wind} kph`;
+    cityElement.textContent = name;
+    imgElement.setAttribute("src", img);
+
+    // Date
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const month = String(currentDate.getMonth() + 1).padStart(2, "0");
+    const day = String(currentDate.getDate()).padStart(2, "0");
+    dateElement.textContent = `(${year}-${month}-${day})`;
+};
+
+const fetchWheatherDetails = async (cityName) => {
+    try {
+        const response = await fetch(
+            `http://api.weatherapi.com/v1/current.json?key=5e790bf48b9d486d99b171939250204&q=${cityName}&aqi=no`
+        );
+        const result = await response.json();
+
+        if (result.error) {
+            setErrorMessage(result.error.message);
+            return;
+        }
+
+        const { name } = result.location;
+        const {
+            temp_c: temperature,
+            humidity,
+            wind_kph: wind,
+        } = result.current;
+
+        const { icon } = result.current.condition;
+
+        // console.log(temperature);
+        UpdateUI(name, temperature, humidity, wind, icon);
+    } catch (error) {
+        // console.log(error);
+        setErrorMessage("Unable to retrieve your location.");
+    }
+};
+
+const fetchWheatherForecast = async (cityName) => {
+    try {
+        const response = await fetch(
+            `http://api.weatherapi.com/v1/forecast.json?key=5e790bf48b9d486d99b171939250204&q=${cityName}&days=6&aqi=no&alerts=no`
+        );
+        const result = await response.json();
+
+        if (result.error) {
+            setErrorMessage(result.error.message);
+            return;
+        }
+
+        const { forecastday: forecast } = result.forecast;
+
+        cardsContainer.innerHTML = "";
+
+        forecast.forEach((f, idx) => {
+            if (idx === 0) return;
+            const el = document.createElement("div");
+            el.classList = "card rounded-lg h-full p-4 border min-w-56";
+            el.innerHTML = `
+                <h3>(${f.date})</h3>
+                <span class="w-14 h-14 inline-block bg-white p-1 rounded-sm mt-2">
+                    <img class="h-full object-contain" src="${f.day.condition.icon}">
+                </span>
+                <div class="mt-2 flex flex-col gap-1">
+                    <h4 class="text-md">Temperature: <span class="temp">${f.day.avgtemp_c} c</span></h4>
+                    <h4 class="text-md">Wind: <span class="wind">${f.day.maxwind_kph} kph</span></h4>
+                    <h4 class="text-md">Humidity: <span class="humidity">${f.day.avghumidity}</span></h4>
+                </div>
+            `;
+            cardsContainer.appendChild(el);
+        });
+    } catch (error) {
+        // console.log(error);
+        setErrorMessage("Unable to retrieve your location.");
+    }
+};
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    errorElement.classList.add("hidden");
+    errorElement.classList.remove("inline-block");
+
+    const formData = new FormData(form);
+
+    const cityName = formData.get("name");
+    if (!cityName) return;
+
+    fetchWheatherDetails(cityName);
+    fetchWheatherForecast(cityName);
+});
+
+useLocationBtn.addEventListener("click", () => {
+    if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition(success, error);
+    } else {
+        setErrorMessage("Geolocation is not supported by this browser.");
+    }
+
+    errorElement.classList.add("hidden");
+    errorElement.classList.remove("inline-block");
+
+    function success(position) {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        // console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
+        fetchWheatherDetails(`${latitude},${longitude}`);
+        fetchWheatherForecast(`${latitude},${longitude}`);
+    }
+
+    function error() {
+        setErrorMessage("Unable to retrieve your location.");
+    }
+});
